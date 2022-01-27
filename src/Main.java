57c94145776e67748f519ec3ad284f066670b961
AlexVladimirov05690 <93385788+AlexVladimirov05690@users.noreply.github.com>
2022-01-27 22:49:09 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        Wish_Ball wish_ball = new Wish_Ball();
+        wish_ball.start();
+        System.out.println(wish_ball.answer);
+        System.out.println(wish_ball.answer);
+        System.out.println(wish_ball.answer);
+        System.out.println(wish_ball.answer);
+        System.out.println(wish_ball.answer);
+        System.out.println(wish_ball.answer);
+        System.out.println(wish_ball.answer);
+        System.out.println(wish_ball.answer);
+
+    }
+}
